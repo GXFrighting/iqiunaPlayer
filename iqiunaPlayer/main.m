cbdc4ae38aa7b43e2825f70347bfeadaa1537ad0
JGX <157610665@qq.com>
2017-09-08 14:32:48 +0800

    集成SDL

--- iqiunaPlayer/main.m
+++ iqiunaPlayer/main.m
@@ -8,9 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#include <SDL.h>
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    
+    return 0;
 }
