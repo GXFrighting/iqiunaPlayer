6d946ccedf8f16a4d54004c10ec3cb85dd0ea2e8
JGX <157610665@qq.com>
2017-09-08 10:10:06 +0800

    init

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iqiunaPlayer' do
+  # use_frameworks!
+  inhibit_all_warnings!
+
+end
---
+++ iqiunaPlayer/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  iqiunaPlayer
+//
+//  Created by Jiang on 2017/8/30.
+//  Copyright © 2017年 Jiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
